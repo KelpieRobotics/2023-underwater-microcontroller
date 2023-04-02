4d06459f9ae7215cfe52fb6dbed7e94747f8e34d
Adam Dia <43157907+dooplu@users.noreply.github.com>
2023-04-01 22:34:29 -0400

    moving over constants

--- kelpie-f446/Core/UserDrivers/MS5837Driver.c
+++ kelpie-f446/Core/UserDrivers/MS5837Driver.c
@@ -3,4 +3,28 @@
  *
  *  Created on: Apr 1, 2023
  *      Author: Adam
+ *
+ *  Adapted from: https://github.com/bluerobotics/BlueRobotics_MS5837_Library
  */
+
+#include "MS5837Driver.h"
+
+
+const uint8_t MS5837_ADDR = 0x76;
+const uint8_t MS5837_RESET = 0x1E;
+const uint8_t MS5837_ADC_READ = 0x00;
+const uint8_t MS5837_PROM_READ = 0xA0;
+const uint8_t MS5837_CONVERT_D1_8192 = 0x4A;
+const uint8_t MS5837_CONVERT_D2_8192 = 0x5A;
+
+const float Pa = 100.0f;
+const float bar = 0.001f;
+const float mbar = 1.0f;
+
+const uint8_t MS5837_30BA = 0;
+const uint8_t MS5837_02BA = 1;
+const uint8_t MS5837_UNRECOGNISED = 255;
+
+const uint8_t MS5837_02BA01 = 0x00; // Sensor version: From MS5837_02BA datasheet Version PROM Word 0
+const uint8_t MS5837_02BA21 = 0x15; // Sensor version: From MS5837_02BA datasheet Version PROM Word 0
+const uint8_t MS5837_30BA26 = 0x1A; // Sensor version: From MS5837_30BA datasheet Version PROM Word 0
--- kelpie-f446/Core/UserDrivers/MS5837Driver.h
+++ kelpie-f446/Core/UserDrivers/MS5837Driver.h
@@ -3,4 +3,8 @@
  *
  *  Created on: Apr 1, 2023
  *      Author: Adam
+ *
+ *    Adapted from: https://github.com/bluerobotics/BlueRobotics_MS5837_Library
  */
+
+#include "UserTypes.h"
