b6557ac287261f4f0757d6b0f642f3d4a81c8f4b
Adam Dia <43157907+dooplu@users.noreply.github.com>
2023-04-11 18:08:50 -0400

    Update NavigationTask.c

--- kelpie-f446/Core/Tasks/NavigationTask.c
+++ kelpie-f446/Core/Tasks/NavigationTask.c
@@ -33,7 +33,7 @@ PUBLIC void InitNavigationTask(void)
 	NavigationTaskHandle = osThreadNew(NavigationTask, NULL, &NavigationTask_attributes);
     if (MS5837_init() != RESULT_OK)
     {
-    	SerialPrintln("Did not initialize MS5837 correctly.");
+    	SerialPrintln("Did not initialize MS5837 correctly");
     }
 }
 PRIVATE void NavigationTask(void *argument)
@@ -44,14 +44,14 @@ PRIVATE void NavigationTask(void *argument)
 	{
 		cycleTick += TIMER_NAV_TASK;
 		osDelayUntil(cycleTick);
-		SerialDebug(TAG, "Navigation Task Loop");
+		SerialPrintln(TAG, "Navigation Task Loop");
 
-		/*if (MS5837_read() != RESULT_OK)
+		if (MS5837_read() != RESULT_OK)
 		    {
 		    	SerialPrintln("Did not read MS5837 correctly.");
 		    }
 
 		MS5837_getValues(&pressure, &depth);
-		SerialPrintln("Current Pressure: %f", pressure);*/
+		SerialPrintln("Current Pressure: %f", pressure);
 	}
 }
